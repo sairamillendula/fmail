63d0c3e56326f1fdf28aadd1d286318f844f7f59
Shrav Mehta <shravvmehtaa@gmail.com>
2014-06-21 13:30:32 -0700

    inital

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root :to => "visitors#index"
+  resources :users, :only => [:index, :show, :edit, :update ]
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/signin' => 'sessions#new', :as => :signin
+  get '/signout' => 'sessions#destroy', :as => :signout
+  get '/auth/failure' => 'sessions#failure'
+end
